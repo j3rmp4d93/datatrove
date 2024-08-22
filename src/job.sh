8869e4920946c458b738ea982d5b4f06405ea122
kevin <j3rmp4d93@gmail.com>
2024-08-22 13:39:11 +0800

    wait 30 mins before restart

--- src/job.sh
+++ src/job.sh
@@ -3,7 +3,7 @@ PYTHON="/home/u231360/miniconda3/envs/datatrove/bin/python3.10"
 SCRIPT="fineweb-TC.py"
 
 until "$PYTHON" "$SCRIPT" "$1"; do
-    sleep 1  # Optional: wait for a second before retrying
+    sleep 1800  # Optional: wait for a second before retrying
 done
 
 #/home/u231360/miniconda3/envs/datatrove/bin/python3.10 fineweb-TC.py $1
