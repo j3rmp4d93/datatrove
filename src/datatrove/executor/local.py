ff953c62482ba573f0eb52d643b8a1f5e39eddaf
kevin <j3rmp4d93@gmail.com>
2024-08-22 16:43:09 +0800

    use ray instead of multiprocess for local executor

--- src/datatrove/executor/local.py
+++ src/datatrove/executor/local.py
@@ -3,7 +3,7 @@ from copy import deepcopy
 from functools import partial
 from typing import Callable
 
-import multiprocess
+import ray
 
 from datatrove.executor.base import PipelineExecutor
 from datatrove.io import DataFolderLike
@@ -13,25 +13,7 @@ from datatrove.utils.stats import PipelineStats
 
 
 class LocalPipelineExecutor(PipelineExecutor):
-    """Executor to run a pipeline locally
-
-    Args:
-        pipeline: a list of PipelineStep and/or custom lamdba functions
-            with arguments (data: DocumentsPipeline, rank: int,
-            world_size: int)
-        tasks: total number of tasks to run the pipeline on (default: 1)
-        workers: how many tasks to run simultaneously. (default is -1 for no limit aka tasks)
-        logging_dir: where to save logs, stats, etc. Should be parsable into a datatrove.io.DataFolder
-        skip_completed: whether to skip tasks that were completed in
-            previous runs. default: True
-        start_method: method to use to spawn a multiprocessing Pool (default: "forkserver")
-        local_tasks: how many of the total tasks should be run on this node/machine. -1 for all
-        local_rank_offset: the rank of the first task to run on this machine.
-            Tasks [local_rank_offset, local_rank_offset + local_tasks] will be run.
-        depends: another LocalPipelineExecutor that should run
-            before this one
-        randomize_start_duration: the maximum number of seconds to delay the start of each task.
-    """
+    """Executor to run a pipeline locally using Ray"""
 
     def __init__(
         self,
@@ -41,7 +23,6 @@ class LocalPipelineExecutor(PipelineExecutor):
         logging_dir: DataFolderLike = None,
         depends: "LocalPipelineExecutor" = None,
         skip_completed: bool = True,
-        start_method: str = "forkserver",
         local_tasks: int = -1,
         local_rank_offset: int = 0,
         randomize_start_duration: int = 0,
@@ -49,7 +30,6 @@ class LocalPipelineExecutor(PipelineExecutor):
         super().__init__(pipeline, logging_dir, skip_completed, randomize_start_duration)
         self.tasks = tasks
         self.workers = workers if workers != -1 else tasks
-        self.start_method = start_method
         self.local_tasks = local_tasks if local_tasks != -1 else tasks
         self.local_rank_offset = local_rank_offset
         self.depends = depends
@@ -59,42 +39,14 @@ class LocalPipelineExecutor(PipelineExecutor):
             )
         self._launched = False
 
-    def _launch_run_for_rank(self, rank: int, ranks_q, completed=None, completed_lock=None) -> PipelineStats:
-        """
-            Small wrapper around _run_for_rank with a queue of available local ranks.
-        Args:
-            rank: rank to run pipeline for
-            ranks_q: queue of local ranks
-            completed: counter with the number of complete tasks
-            completed_lock: lock to synchronize completed counter
-
-        Returns: the stats for this task
-
-        """
-        local_rank = ranks_q.get()
-        try:
-            return self._run_for_rank(rank, local_rank)
-        finally:
-            if completed and completed_lock:
-                with completed_lock:
-                    completed.value += 1
-                    logger.info(f"{completed.value}/{self.world_size} tasks completed.")
-            ranks_q.put(local_rank)  # free up used rank
+        # Initialize Ray
+        ray.init(ignore_reinit_error=True, num_cpus=self.workers)
 
     def run(self):
         """
-            This method is responsible for correctly invoking `self._run_for_rank` for each task that is to be run.
-
-            On a LocalPipelineExecutor, this method will spawn a multiprocess pool if workers != 1.
-            Otherwise, ranks will be run sequentially in a loop.
-        Returns:
-
+            Run the pipeline tasks using Ray for parallelism.
         """
-        assert not self.depends or (
-            isinstance(self.depends, LocalPipelineExecutor)
-        ), "depends= must be a LocalPipelineExecutor"
         if self.depends:
-            # take care of launching any unlaunched dependencies
             if not self.depends._launched:
                 logger.info(f'Launching dependency job "{self.depends}"')
                 self.depends.run()
@@ -108,11 +60,7 @@ class LocalPipelineExecutor(PipelineExecutor):
             return
 
         self.save_executor_as_json()
-        mg = multiprocess.Manager()
-        ranks_q = mg.Queue()
-        for i in range(self.workers):
-            ranks_q.put(i)
-
+        
         ranks_to_run = self.get_incomplete_ranks(
             range(self.local_rank_offset, self.local_rank_offset + self.local_tasks)
         )
@@ -124,23 +72,24 @@ class LocalPipelineExecutor(PipelineExecutor):
             stats = []
             for rank in ranks_to_run:
                 self.pipeline = deepcopy(pipeline)
-                stats.append(self._launch_run_for_rank(rank, ranks_q))
+                stats.append(self._run_for_rank(rank, 0))
         else:
-            completed_counter = mg.Value("i", skipped)
-            completed_lock = mg.Lock()
-            ctx = multiprocess.get_context(self.start_method)
-            with ctx.Pool(self.workers) as pool:
-                stats = list(
-                    pool.imap_unordered(
-                        partial(
-                            self._launch_run_for_rank,
-                            ranks_q=ranks_q,
-                            completed=completed_counter,
-                            completed_lock=completed_lock,
-                        ),
-                        ranks_to_run,
-                    )
-                )
+            @ray.remote(max_retries=-1)#will keep trying when failed, wait 30 mins to prevent ddos.
+            def _run_for_rank_ray(rank: int, local_rank: int):
+                try:
+                    return self._run_for_rank(rank, local_rank)  
+                except Exception as e:
+                    print(f"Task failed with error: {e}. wait 30mins before proceeding.")
+                    time.sleep(1800)
+                    raise e
+
+            actors = [_run_for_rank_ray for _ in range(self.workers)]
+            actor_pool = ray.util.ActorPool(actors)
+            for i, rank in enumerate(ranks_to_run):
+                actor_pool.submit(lambda agent, v: agent.remote(v, local_rank=0), i)
+            stats =[]
+            while actor_pool.has_next():
+                stats.append(actor_pool.get_next(timeout=None))
         # merged stats
         stats = sum(stats, start=PipelineStats())
         with self.logging_dir.open("stats.json", "wt") as statsfile:
@@ -150,9 +99,4 @@ class LocalPipelineExecutor(PipelineExecutor):
 
     @property
     def world_size(self) -> int:
-        """
-            Simply the number of tasks
-        Returns:
-
-        """
         return self.tasks
--- src/job.sh
+++ src/job.sh
@@ -1,9 +1,10 @@
 cd datatrove/src/
+conda activate /home/u231360/miniconda3/envs/datatrove
 PYTHON="/home/u231360/miniconda3/envs/datatrove/bin/python3.10"
 SCRIPT="fineweb-TC.py"
-
-until "$PYTHON" "$SCRIPT" "$1"; do
-    sleep 1800  # Optional: wait for a second before retrying
-done
+"$PYTHON" "$SCRIPT" "$1"
+#until "$PYTHON" "$SCRIPT" "$1"; do
+    #sleep 1800  # Optional: wait for a second before retrying
+#done
 
 #/home/u231360/miniconda3/envs/datatrove/bin/python3.10 fineweb-TC.py $1
