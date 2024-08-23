6ca8a04dea3023e3f39659b6f291e09673fe969a
kevin <j3rmp4d93@gmail.com>
2024-08-23 11:14:08 +0800

    renew launch script on devcloud

--- src/job.sh
+++ src/job.sh
@@ -2,7 +2,7 @@ cd datatrove/src/
 conda activate /home/u231360/miniconda3/envs/datatrove
 PYTHON="/home/u231360/miniconda3/envs/datatrove/bin/python3.10"
 SCRIPT="fineweb-TC.py"
-"$PYTHON" "$SCRIPT" "$1"
+"$PYTHON" "$SCRIPT" "$1" "$2"
 #until "$PYTHON" "$SCRIPT" "$1"; do
     #sleep 1800  # Optional: wait for a second before retrying
 #done
